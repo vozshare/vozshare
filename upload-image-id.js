907f24cfd59fba14f7582a64690d1014d1b86560
vozshare <100843366+vozshare@users.noreply.github.com>
2022-03-12 20:54:13 +0700

    Update upload-image-id.js

--- upload-image-id.js
+++ upload-image-id.js
@@ -1,4 +1,4 @@
-/* Imgur ID Script - Truong Blogger*/
+/* Imgur ID Script - vozshare.com*/
 var feedback = function(c) {
     !0 === c.success && (c = c.data.link.replace(/^http:\/\//i, "https://"), document.querySelector(".status").classList.add("bg-success"), document.querySelector(".status").innerHTML = '<div class="linkimg"><input class="image-url" id="copylinkimg" onclick="this.select()" value="' + c + '"/></div><div class="showimg"><img class="img" alt="Imgur-Upload" src="' + c + '"/></div>')
 };
